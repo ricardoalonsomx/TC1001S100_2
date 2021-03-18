545596c50311b102f3b115fc0dfccf3b7afb2f72
ArturoSosaC <A01022687@itesm.mx>
2021-03-18 11:48:39 -0600

    Make snake go around the edges

--- snake.py
+++ snake.py
@@ -31,7 +31,17 @@ def move():
     head = snake[-1].copy()
     head.move(aim)
 
-    if not inside(head) or head in snake:
+    if not inside(head):
+        if head.x > 190:
+            head.x = -200
+        elif head.x < -200:
+            head.x = 190
+        elif head.y > 190:
+            head.y = -200
+        elif head.y < -200:
+            head.y = 190
+
+    elif head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
