ce07e94aa1c2ca2cd38e4a3d1e8f265e1276a0cd
Ricardo Alonso <aldoricardoalonsoarostegui@MacBook-Pro-de-Ricardo-2.local>
2021-03-18 20:08:49 -0600

    Corrección de commit, puesto que accidentalmente fue subido en la branch de Arturo | Añadidos ejercicios 1, 2, 3 y 4, pendientes de corrección.

--- memory.py
+++ memory.py
@@ -1,4 +1,5 @@
-"""Memory, puzzle game of number pairs.
+"""
+Memory, puzzle game of number pairs.
 
 Exercises:
 
@@ -8,16 +9,57 @@ Exercises:
 4. Center single-digit tile.
 5. Use letters instead of tiles.
 
+Excercises solved by Ricardo Alonso
+18/03/21.
 """
 
 from random import *
 from turtle import *
 from freegames import path
 
+while True:
+    length = int(input("Please insert the size of the grid sides: ")) if input("Type Y if you want a custom size grid: ") == 'Y' else 4 # This variable manages how many rows/columns the grid will have, , accomplishing what EXERCISE 2 specifies.
+    
+    if length % 2 != 0:
+        print("The grid side size must be an even number\n")
+    elif length > 10:
+        print("The grid side size must be smaller or equal to 10")
+    else:
+        break
+        
+    
+
+area = length ** 2
+remainingTiles = area/2 # We define the remaining tiles as the total tiles over 2.
 car = path('car.gif')
-tiles = list(range(32)) * 2
+tiles = list(range(int(remainingTiles)))
 state = {'mark': None}
-hide = [True] * 64
+hide = [True] * area
+taps = 0
+xPixels = 400 / length
+useLetters = True if input("Type L if you want to use letters instead of numbers: ") == 'L' else False
+
+print(tiles)
+
+if useLetters: # If user decided to use letters we make the tiles list of letters, , accomplishing what EXERCISE 5 specifies.
+    seed(1)
+
+    for i in range (len(tiles)):
+        while True: # This while cycle will make sure we are using only letters, and that none of them are repeating
+            element = tiles[i] if type(tiles[i]) is int else randrange(65, 90)
+            print("Element = " + str(element))
+
+            if (element >= 65 and element <= 90) or (element >= 97 and element <= 122):
+                char = chr(element)
+            else:
+                char = chr(randrange(65, 90))
+
+            if not (char in tiles):
+                tiles[i] = char
+                break
+
+tiles *= 2
+            
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
@@ -27,22 +69,26 @@ def square(x, y):
     color('black', 'white')
     begin_fill()
     for count in range(4):
-        forward(50)
+        forward(xPixels)
         left(90)
     end_fill()
 
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
-    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+    return int((x + 200) // xPixels + ((y + 200) // xPixels) * length)
 
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
-    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+    return (count % length) * xPixels - 200, (count // length) * xPixels - 200
 
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
+    global taps, remainingTiles
+
     spot = index(x, y)
     mark = state['mark']
+    taps += 1 # Adds 1 to the count of taps, accomplishing what EXERCISE 1 specifies.
+    print(taps)
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
@@ -50,6 +96,11 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        
+        if remainingTiles <= 1: # If there's not remaining tiles then it notifies you that you won, accomplishing what EXERCISE 3 specifies.
+            print("You won!\nYou made %d movements" % (taps))
+        else: # Else it just subtract a tile to the remaining tiles count.
+            remainingTiles -= 1
 
 def draw():
     "Draw image and tiles."
@@ -58,7 +109,7 @@ def draw():
     shape(car)
     stamp()
 
-    for count in range(64):
+    for count in range(area):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
@@ -68,7 +119,10 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        if len(str(tiles[mark])) == 1:
+            goto(x + (xPixels / 2 - 5), y) # If the length of the tile equals 1 it goes to a centered position, accomplishing what EXERCISE 4 specifies.
+        else:
+            goto(x + 5, y)
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
